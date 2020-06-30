af63f42a9827dc106dd6fb846a706970a9030b3f
unknown <gbadamosiopeyemi@gmail.com>
2020-06-30 18:12:41 +0100

    created models for todoapp

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Task(models.Model):
+    description = models.CharField(max_length = 200)
+    due_date = models.DateField(auto_now = True)
+    created_at = models.DateField(auto_now =True)
+    category = models.ForeignKey(Category, default = "general", on_delete = models.CASCADE)
