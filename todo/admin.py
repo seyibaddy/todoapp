df4688866f6f0b8be3c8b9302f99735310e75706
unknown <gbadamosiopeyemi@gmail.com>
2020-07-01 17:02:02 +0100

    category and task added

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category,Task
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Task,TaskAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+
+    def __str__(self):
+        return self.name
 
 class Task(models.Model):
     description = models.CharField(max_length = 200)
